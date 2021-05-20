7aed5eacd62aae43f5c87be28319fe70cf7fe057
ktechen <kevHTW@gmx.de>
2021-05-20 13:36:32 +0200

    Add new style to registrations.scss

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < ApplicationController
   def index
-    render nothing: true
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sven/index'
+  get 'sven/help'
+  get 'sven/intern'
   get 'registrations/index'
   get 'home_pages/index'
   root 'home_pages#index'
