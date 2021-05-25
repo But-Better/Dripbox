afcb0db2de34c93d1b7318ed052edf7db7fbe098
ktechen <kevHTW@gmx.de>
2021-05-25 16:21:37 +0200

    Add route for main page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'registrations', to: "registrations#index"
   get 'registrations/index'
   get 'home_pages/index'
+  get "login", to: "home_pages#index"
   root 'home_pages#index'
 
 end
