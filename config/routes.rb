87c2a59efa948633ed38c35eab0219a458d5ca67
ktechen <kevHTW@gmx.de>
2021-05-27 22:14:32 +0200

    Remove to short routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'registrations', to: "registrations#index"
   get 'registrations/index'
-  get "r", to: "registrations#index"
-
   get 'home_pages/index'
-  get "login", to: "home_pages#index"
-  get "signin", to: "home_pages#index"
-  get "sign-in", to: "home_pages#index"
   root 'home_pages#index'
 
 end
