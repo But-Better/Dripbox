d1f86f659c2842c5b76bfcfb71743b0b9be75749
Note <kitsu-geissler@t-online.de>
2021-06-02 19:59:59 +0200

    module refactoring for heroku testing

--- db/migrate/20210602144354_create_file_resource.rb
+++ db/migrate/20210602144354_create_file_resource.rb
@@ -1,6 +1,6 @@
 class CreateFileResources < ActiveRecord::Migration[6.1]
   def change
-    create_table :file_resources do |t|
+    create_table :file_resource do |t|
       t.string :name
       t.string :url
       t.date :upload
--- db/migrate/20210602144401_create_category.rb
+++ db/migrate/20210602144401_create_category.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
   def change
-    create_table :categories do |t|
+    create_table :category do |t|
       t.string :title
       t.text :desc
 
