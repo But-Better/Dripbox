f02f63d2396e894d9eed2cf0ee6add0f3d7a2b36
ktechen <kevHTW@gmx.de>
2021-06-04 18:28:40 +0200

    Add First Tests and Add Validation via Rails

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2021_05_27_200903) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string "username", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
