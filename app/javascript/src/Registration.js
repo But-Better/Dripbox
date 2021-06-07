8a222e02a30efc854ba477c0b4073c6816aef35b
ktechen <kevHTW@gmx.de>
2021-06-08 00:28:48 +0200

    Import home.js

--- app/javascript/src/Registration.js
+++ app/javascript/src/Registration.js
@@ -1,14 +1,14 @@
 import {
-    isEqual,
-    isLengthCorrect,
-    lowercaseRegex,
-    numberRegex, showPassword,
-    specialRegex,
-    uppercaseRegex
+  isEqual,
+  isLengthCorrect,
+  lowercaseRegex,
+  numberRegex, showPassword,
+  specialRegex,
+  uppercaseRegex
 } from "./PasswordHandler";
 
-let password = document.getElementById("passwordId");
-let passwordAgain = document.getElementById("passwordAgainId");
+let password = "";
+let passwordAgain = "";
 
 const plus = "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z";
 const minus = "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z";
@@ -16,83 +16,81 @@ const minus = "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5
 const min_height = 1001;
 
 function movePasswordRequirementHTML() {
-    window.addEventListener("change", () => {
-        const height = window.screen.height;
-        const alert = document.getElementById("container-alert");
-        if (height >= min_height) {
-            alert.remove();
-        }
-    });
+  window.addEventListener("change", () => {
+    const height = window.screen.height;
+    const alert = document.getElementById("container-alert");
+    if (height >= min_height) {
+      alert.remove();
+    }
+  });
 }
 
 function validationHandler(id, value) {
-    const svg = document.getElementById(id);
+  const svg = document.getElementById(id);
 
-    if (value) {
-        svg.style.fill = "#2ECC40";
-        svg.setAttribute("d", plus);
-    } else {
-        svg.style.fill = "#FF4136";
-        svg.setAttribute("d", minus);
-    }
+  if (value) {
+    svg.style.fill = "#2ECC40";
+    svg.setAttribute("d", plus);
+  } else {
+    svg.style.fill = "#FF4136";
+    svg.setAttribute("d", minus);
+  }
 }
 
 function passwordEquals() {
-    let fun = () => {
-        const btn = document.getElementById("regBtn")
-        let result = isEqual(password.value, passwordAgain.value);
-        if (result) {
-            btn.removeAttribute("disabled");
-        } else {
-            btn.setAttribute("disabled", "true");
-        }
+  let fun = () => {
+    const btn = document.getElementById("regBtn")
+    let result = isEqual(password.value, passwordAgain.value);
+    if (result) {
+      btn.removeAttribute("disabled");
+    } else {
+      btn.setAttribute("disabled", "true");
     }
+  }
 
-    passwordAgain.addEventListener("keyup", () => {
-        fun();
-    })
+  passwordAgain.addEventListener("keyup", () => {
+    fun();
+  })
 
-    password.addEventListener("keyup", () => {
-        fun();
-    })
+  password.addEventListener("keyup", () => {
+    fun();
+  })
 
 }
 
 function eventHandler(event) {
-    if (event.type === 'keyup') {
+  if (event.type === 'keyup') {
 
-    }
+  }
 }
 
 function events() {
 
-    password.addEventListener("keyup", () => {
-        validationHandler("lowercaseSVGPath", lowercaseRegex(password.value));
-        validationHandler("uppercaseSVGPath", uppercaseRegex(password.value));
-        validationHandler("numberSVGPath", numberRegex(password.value));
-        validationHandler("specialCharSVGPath", specialRegex(password.value));
-        validationHandler("lengthSVGPath", isLengthCorrect(password.value));
-        validationHandler("equalSVGPath", isEqual(password.value, passwordAgain.value));
-    });
+  password.addEventListener("keyup", () => {
+    validationHandler("lowercaseSVGPath", lowercaseRegex(password.value));
+    validationHandler("uppercaseSVGPath", uppercaseRegex(password.value));
+    validationHandler("numberSVGPath", numberRegex(password.value));
+    validationHandler("specialCharSVGPath", specialRegex(password.value));
+    validationHandler("lengthSVGPath", isLengthCorrect(password.value));
+    validationHandler("equalSVGPath", isEqual(password.value, passwordAgain.value));
+  });
 
-    passwordAgain.addEventListener("keyup", () => {
-        validationHandler("equalSVGPath", isEqual(password.value, passwordAgain.value));
-    });
+  passwordAgain.addEventListener("keyup", () => {
+    validationHandler("equalSVGPath", isEqual(password.value, passwordAgain.value));
+  });
 }
 
 function init() {
-    password = document.getElementById("passwordId");
-    password.value = "";
+  password = document.getElementById("passwordId");
+  password.value = "";
 
-    passwordAgain = document.getElementById("passwordAgainId");
-    passwordAgain.value = "";
+  passwordAgain = document.getElementById("passwordAgainId");
+  passwordAgain.value = "";
 }
 
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        init();
-        events();
-        passwordEquals()
-        showPassword(document.getElementById("Reg_ShowPassword"), password)
-    }
-}
+document.addEventListener("turbolinks:load", () => {
+  init();
+  events();
+  passwordEquals();
+  showPassword(document.getElementById("Reg_ShowPassword"), password);
+})
--- app/javascript/src/home.js
+++ app/javascript/src/home.js
@@ -3,46 +3,44 @@ const {showPassword} = require("./PasswordHandler");
 let password = document.getElementById("floatingPassword");
 
 function animationOfImage() {
-    const image = document.getElementById("mainImgId");
-    const email = document.getElementById("floatingInput");
+  const image = document.getElementById("mainImgId");
+  const email = document.getElementById("floatingInput");
 
-    const defaultSrc = image.src;
+  const defaultSrc = image.src;
 
-    email.addEventListener("focusin", () => {
-        image.setAttribute("src", "https://c.tenor.com/pBrzvwLzbwoAAAAi/hacking-hack.gif");
-    })
+  email.addEventListener("focusin", () => {
+    image.setAttribute("src", "https://c.tenor.com/pBrzvwLzbwoAAAAi/hacking-hack.gif");
+  })
 
-    document.addEventListener("focusout", () => {
-        image.setAttribute("src", defaultSrc);
-    })
+  document.addEventListener("focusout", () => {
+    image.setAttribute("src", defaultSrc);
+  })
 
-    password.addEventListener("focusin", () => {
-        image.setAttribute("src", "https://c.tenor.com/pBrzvwLzbwoAAAAi/hacking-hack.gif");
-    });
+  password.addEventListener("focusin", () => {
+    image.setAttribute("src", "https://c.tenor.com/pBrzvwLzbwoAAAAi/hacking-hack.gif");
+  });
 
 }
 
 function moveImage() {
-    const id = document.getElementById("mainImgId");
-    const x = 0;
-    document.addEventListener("mousemove", (e) => {
-        id.setAttribute("transform", "translate3d(" + e.clientX + "," + e.clientY + "," + 0 + ")");
-        id.style.position = 'absolute';
-        id.style.left = e.clientX + "px";
-        id.style.top = e.clientY + "px";
-    });
+  const id = document.getElementById("mainImgId");
+  const x = 0;
+  document.addEventListener("mousemove", (e) => {
+    id.setAttribute("transform", "translate3d(" + e.clientX + "," + e.clientY + "," + 0 + ")");
+    id.style.position = 'absolute';
+    id.style.left = e.clientX + "px";
+    id.style.top = e.clientY + "px";
+  });
 }
 
-function init(){
-    password = document.getElementById("floatingPassword");
-    password.value = "";
+function init() {
+  password = document.getElementById("floatingPassword");
+  password.value = "";
 }
 
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        init();
-        const showSwitch = document.getElementById("ShowSwitch");
-        animationOfImage();
-        showPassword(showSwitch, password);
-    }
-}
+window.addEventListener("load", () => {
+  init();
+  const showSwitch = document.getElementById("ShowSwitch");
+  animationOfImage();
+  showPassword(showSwitch, password);
+})
