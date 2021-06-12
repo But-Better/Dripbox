5cb8475b7df43acfb99e34b4c8fc3be4d2daa010
Note <kitsu-geissler@t-online.de>
2021-06-12 21:48:34 +0200

    Rewrite FileResource to Resource

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @files = FileResource.all
+    @files = Resource.all
     @categories = Category.all
     return unless params.key?('id')
 
-    @called_file = FileResource.find params[:id]
+    @called_file = Resource.find params[:id]
     @user_name = @called_file.user_id
     @called_file_cats = Category.where({ id: FileResourceCategoryRelation.where({ file_resource_id: params[:id] }) })
   end
