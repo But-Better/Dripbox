cb833cc6709dede93cfcf1ce7e53854f34ff3740
ktechen <kevHTW@gmx.de>
2021-06-14 21:45:29 +0200

    Add missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   get 'registrations/index', to: 'users#new', as: 'signup'
   get 'home_pages/index', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/dashboard', to: 'dashboard#index'
+  get '/dashboard/:id', to: 'dashboard#index'
+
+  get '/agb',         to: 'site_informations#agb'
+  get '/datenschutz', to: 'site_informations#datenschutz'
+  get '/uber',        to: 'site_informations#about'
+  get '/ueber',       to: 'site_informations#about'
+
   root 'home_pages#index'
 
   resources :users
