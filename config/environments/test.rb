57f3da7ed9c9a10748aa149c23de659acdba571a
ktechen <kevHTW@gmx.de>
2021-06-16 21:53:57 +0200

    Add Config for Emailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
