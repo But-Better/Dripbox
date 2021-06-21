cea5842051a5adfa5510475f5c8f3f8a470c116c
ktechen <kevHTW@gmx.de>
2021-06-21 21:37:20 +0200

    Change redirect_to to render and add Visual Img

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,11 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         redirect_to dashboard_path
       else
-        render 'user_mailer/Index'
+        render :new
       end
     else
       flash[:alert] = 'Email or password was invalid'
-      redirect_to root_url
+      render :new
     end
 
   end
