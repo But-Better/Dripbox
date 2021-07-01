468d1e38c8bdfa66b7f70e498c859db044a4641c
ktechen <kevHTW@gmx.de>
2021-07-01 15:56:53 +0200

    Add Login test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,18 +3,26 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get new' do
-    get sessions_new_url
+
+
+  test 'Login' do
+    password = 'Admin123'
+    username = '10Head'
+    user = User.create(username: username, email: 'Admin@5head.de', password: password, password_confirmation: password)
+
+    post :"/login", params: { email: user[:email], password: user[password] }
+
+    assert_not_equal session[:id], user[:id]
     assert_response :success
   end
 
-  test 'should get create' do
-    get sessions_create_url
-    assert_response :success
-  end
+  test 'Wrong Login' do
+    password = 'Admin123'
+    email = '10Head@bbg.com'
 
-  test 'should get destroy' do
-    get sessions_destroy_url
+    post :"/login", params: { email: [email], password: [password] }
+
+    assert_not_equal session[:id], user[:id]
     assert_response :success
   end
 end
