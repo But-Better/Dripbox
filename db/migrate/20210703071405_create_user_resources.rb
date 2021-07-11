bf841e75f1208276fb76d57bd6ae416ce52a3f7b
NoteFx <s0569194@htw-berlin.de>
2021-07-11 11:30:56 +0200

    git rebase merge conflict resolution

--- db/migrate/20210703071405_create_user_resources.rb
+++ db/migrate/20210703071405_create_user_resources.rb
@@ -3,10 +3,11 @@
 class CreateUserResources < ActiveRecord::Migration[6.1]
   def change
     create_table :user_resources do |t|
+      t.belongs_to :user
+      t.has_many :tags
+
       t.string :name
       t.text :desc
-
-      t.belongs_to :user
 
       t.timestamps
     end
