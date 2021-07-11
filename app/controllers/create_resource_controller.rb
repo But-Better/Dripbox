0a7851dca146fbafcca8d45f814e2ebd3777cbdd
NoteFx <s0569194@htw-berlin.de>
2021-07-11 11:30:14 +0200

    remove old un-needed files

--- app/controllers/create_resource_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# create resource controller
-class CreateResourceController < ApplicationController
-  def index
-    @resources = Resource.all
-    render 'resources/index'
-  end
-end
