492958b1c8c8c4543aa8326fdc82876e69a1c501
NoteFx <s0569194@htw-berlin.de>
2021-07-13 15:35:27 +0200

    add user_resource.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,5 @@
-# frozen_string_literal: true
-
-class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def create
-    @user = User.new(user_params)
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+class UserResourcesController < ApplicationController
+  def index
+    render 'registrations'
   end
 end
