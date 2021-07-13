fb8a71899a89788d18da54f5d23a452f8fff0b4b
NoteFx <s0569194@htw-berlin.de>
2021-07-13 15:28:15 +0200

    reformatting commit

--- db/migrate/20210703071405_create_user_resources.rb
+++ db/migrate/20210703071405_create_user_resources.rb
@@ -3,10 +3,10 @@
 class CreateUserResources < ActiveRecord::Migration[6.1]
   def change
     create_table :user_resources do |t|
-      t.belongs_to :user
-
       t.string :name
       t.text :desc
+
+      t.belongs_to :user
 
       t.timestamps
     end
