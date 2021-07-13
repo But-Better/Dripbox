a9c7481542c417783f70508b78c6e10610d293b0
NoteFx <s0569194@htw-berlin.de>
2021-07-13 15:38:11 +0200

    add tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
+  has_and_belongs_to_many :user_resources
+
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            allow_nil: false,
+            allow_blank: false
 end
