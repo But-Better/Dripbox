f6e399ccc4f1bdb080626b248545a0cbd527a72f
MathisHTW <mathis.schott@gmail.com>
2021-07-19 05:22:30 -0400

    Changes by notefox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  require 'uri'
+  # has_secure_password
 
   has_many :user_resources
+
+  validates :username,
+            uniqueness: true,
+            presence: true,
+            length: { minimum: 2, maximum: 20 },
+            allow_nil: false
 
 end
