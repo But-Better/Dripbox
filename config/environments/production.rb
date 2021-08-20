6bee64b0eba7df87d07427c7136c6c28d63330b6
Ktechen <57128890+Ktechen@users.noreply.github.com>
2021-08-20 11:58:12 +0200

    try to fix again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { :host => 'dripbox.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
-    address: 'smtp.gmail.com',
+    address: 'gmail.com',
     user_name: ENV['Gmail'],
     password: ENV['Gmail-PW'],
     authentication: :plain,
