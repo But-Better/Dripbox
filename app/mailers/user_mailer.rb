d7a56bb8dc63d99bc69878fcdda56f04d86c6fb7
Ktechen <57128890+Ktechen@users.noreply.github.com>
2021-08-20 13:49:33 +0200

    change mail to

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
 
   def registration_confirmation(user)
     @user = user
-    mail(to: "#{user.username} <#{user.email}>", subject: 'Registration Confirmation')
+    mail to: user.email, subject: 'Registration Confirmation'
   end
 
   def forgot_password(user)
