9fcecdfc88715feee53fa45d9e039411dabaed83
Ktechen <57128890+Ktechen@users.noreply.github.com>
2021-08-20 11:53:14 +0200

    Try to fix config for SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'dripbox.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'dripbox.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
