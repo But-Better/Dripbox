a769c517479bc549db09d73b22efcb7930a837e5
Ktechen <57128890+Ktechen@users.noreply.github.com>
2021-08-20 12:10:36 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'dripbox.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
     port: 587,
-    address: 'gmail.com',
+    domain: "gmail.com",
     user_name: ENV['Gmail'],
     password: ENV['Gmail-PW'],
     authentication: :plain,
