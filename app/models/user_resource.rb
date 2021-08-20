55aa328580112108a9bcb9051eeec9afa99c1e05
Note <kitsu-geissler@t-online.de>
2021-08-20 10:56:54 +0200

    update file access

--- app/models/user_resource.rb
+++ app/models/user_resource.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserResource < ApplicationRecord
-  has_one_attached :file, service: :AzureStorage
+  has_one_attached :file, service: :local
   belongs_to :user
   has_and_belongs_to_many :tags
 
