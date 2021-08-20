fe328cb74359063dd4ce5bb2c5a2770828dea597
Ktechen <57128890+Ktechen@users.noreply.github.com>
2021-08-20 12:14:53 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: ENV['Gmail'],
-    password: ENV['Gmail-PW'],
+    user_name: ENV['GMAIL'],
+    password: ENV['GMAIL_PW'],
     authentication: :plain,
     enable_starttls_auto: true
   }
