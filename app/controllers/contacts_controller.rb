1276bd85de590e6d94c13c941debb16059a8f90e
Kevin <Kevin-te@t-online.de>
2021-08-21 18:08:31 +0200

    add simple view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,15 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(contact_params)
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ApplicationRecord
+
+  require 'uri'
+
+  validates :name,
+            length: { minimum: 2, maximum: 30 }
+
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :message,
+            length: {minimum: 1, maximum: 8192}
+
+
 end
