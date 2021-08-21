5886fa4b2604f4cc8ac0a9d9c87135be44e6d04d
Kevin <Kevin-te@t-online.de>
2021-08-21 20:09:14 +0200

    add unittest and fix mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,7 @@ class ContactMailer < ApplicationMailer
   default from: 'dripbox@herokuapp.com'
 
   def send_information(contact)
+    @contact = contact
     mail to: contact.email, subject: 'You has created a contact'
   end
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test_send_information" do
+    name = "Peter Agile"
+    email = "Peter-Meister@Agile.com"
+    message = "Ich habe heute ein Contact formular erstellt und bug frei implementiert und es geht auch !!!"
+
+    contact = Contact.create(name: name, email: email, message: message)
+    send_email = ContactMailer.send_information(contact).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_equal [contact.email], send_email.to
+
+  end
 end
