30c0b2baddaff553e0e0764ff25b98bd8aaebb7d
Kevin <Kevin-te@t-online.de>
2021-08-25 09:19:49 -0700

    add lang for en/de/pepe for footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   def logged_in?
     !!session[:user_id]
   end
