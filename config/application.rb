aca2b5c9173f49204f5a3b3ee15f8308caf11cd1
Kevin <Kevin-te@t-online.de>
2021-08-25 08:46:37 -0700

    add lang for en/de/pepe for login page

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Dripbox
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
