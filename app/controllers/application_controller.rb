d5371272c077f6f3e121c17ac277546c390f2e12
Kevin <Kevin-te@t-online.de>
2021-08-25 10:06:28 -0700

    add load language after redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,27 @@
 
 class ApplicationController < ActionController::Base
   around_action :switch_locale
+  before_action :default_url_options
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def set_locale
+    if params[:locale].present?
+      cookies.permanent[:locale] = params[:locale] # save cookies
+    end
+
+    locale = cookies[:locale]&.to_sym # read cookies
+
+    if I18n.available_locales.include?(locale)
+      I18n.locale = locale # use cookies locale
+    end
   end
 
   def logged_in?
