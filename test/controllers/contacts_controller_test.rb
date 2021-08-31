6d79bb25f0d03158cdd668bc79824958afa51c96
Kevin <Kevin-te@t-online.de>
2021-08-31 08:49:57 -0700

    Add unittests

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -3,13 +3,23 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @contacts = Contact.create(name: "Johannes kann es", email: "KannEs@Johannes.de", message: "i am jo---han---es and show me what your got")
+  end
+
   test 'should get new' do
-    get contacts_new_url
+    get contacts_path
     assert_response :success
   end
 
-  test 'should get create' do
-    get contacts_create_url
+  test 'Create Contact' do
+    post contacts_url, contacts: { contacts: { name: @contacts.name, email: @contacts.email, message: @contacts.message } }
     assert_response :success
   end
+
+  test 'Create Contact and check contactable users' do
+
+  end
+
 end
