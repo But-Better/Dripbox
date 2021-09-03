83a114ba30d1415292fd371c5cbb5fb1a3338d3a
Note <kitsu-geissler@t-online.de>
2021-09-03 19:38:20 +0200

    add dashboard_controller.rb statistics functions update redirect to login when not logged in

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,19 @@
 
 class DashboardController < ApplicationController
   def index
-    redirect_to registrations_index_path unless logged_in?
+    unless logged_in?
+      redirect_to login_path
+      return
+    end
+
+    current_user
+
+    load_last_uploaded_file
+    load_upload_file_history
+    load_number_of_files_per_type
+    load_times_of_login
+    load_total_numbers_of_uploads
+    load_total_upload_size
 
     @files = UserResource.all
     @categories = Tag.all
@@ -15,5 +27,44 @@ class DashboardController < ApplicationController
     return if @called_file.file.filename.nil?
 
     @file_of_current_resource = @called_file.file
+
+  end
+
+  private
+
+  # noinspection RubyNilAnalysis
+  def load_last_uploaded_file
+    @last_uploaded_file = @current_user.user_resources.last
+    @last_uploaded_file = 'none' if @last_uploaded_file.nil?
+  end
+
+  # noinspection RubyNilAnalysis
+  def load_upload_file_history
+    @upload_file_history = @current_user.user_resources.all
+    # TODO: code here
+  end
+
+  # noinspection RubyNilAnalysis
+  def load_number_of_files_per_type
+    @number_of_files_per_type = @current_user.user_resources.all
+    # TODO: code here
+  end
+
+  # noinspection RubyNilAnalysis
+  def load_times_of_login
+    @number_of_logins = 0
+    # TODO: code here
+  end
+
+  # noinspection RubyNilAnalysis
+  def load_total_numbers_of_uploads
+    @total_number_of_uploads = 0
+    # TODO: code here
+  end
+
+  # noinspection RubyNilAnalysis
+  def load_total_upload_size
+    @total_upload_size = 0
+    # TODO: code here
   end
 end
