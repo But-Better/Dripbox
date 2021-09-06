f6ae4e595df61c42014fb9863331a82ace3d467a
Kevin <Kevin-te@t-online.de>
2021-09-06 09:29:32 -0700

    rubocop

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Dripbox
     I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
     # Permitted locales available for the application
-    I18n.available_locales = [:en, :de, :pepe]
+    I18n.available_locales = %i[en de pepe]
 
     # Set default locale to something other than :en
     I18n.default_locale = :en
