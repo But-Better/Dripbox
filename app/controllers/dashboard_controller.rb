43e28f96c1dcd9b7b1ab02aed15a272be78bce07
Note <kitsu-geissler@t-online.de>
2021-09-07 01:26:58 +0200

    update metric graphic for overview (a couple of todos still in there)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -43,12 +43,19 @@ class DashboardController < ApplicationController
 
   # noinspection RubyNilAnalysis
   def load_upload_file_history
-    @upload_file_history = @current_user.user_resources.all
-
+    # TODO: clean up multiple resource fetching
+    uploaded_files = @current_user.user_resources.all
+    @upload_file_history = []
+    # TODO: gets multiple dates
+    all_upload_dates.each do |item|
+      puts "\t- " + item.to_s unless item.nil?
+      @upload_file_history.append({ 'date': item, 'number': get_number_of_files_at_date(item) })
+    end
   end
 
   # noinspection RubyNilAnalysis
   def load_number_of_files_per_type
+    # TODO: clean up multiple resource fetching
     @number_of_files_per_type = @current_user.user_resources.all
     # TODO: code here
   end
@@ -69,5 +76,29 @@ class DashboardController < ApplicationController
   def load_total_upload_size
     @total_upload_size = 0
     # TODO: code here
+  end
+
+  # noinspection RubyNilAnalysis
+  # @return [Array]
+  def all_upload_dates
+    all_upload_dates = []
+    # TODO: clean up multiple resource fetching
+    all_res = @current_user.user_resources.all
+    all_res.each do |item|
+      all_upload_dates.append(item.created_at.to_date)
+    end
+    all_upload_dates.uniq
+  end
+
+  # noinspection RubyNilAnalysis
+  def get_number_of_files_at_date(date)
+    # TODO: clean up multiple resource fetching
+    all_files = @current_user.user_resources.all
+    nof = 0 # number of files
+    # TODO: doesn't find any it, need to find out why
+    all_files.each do |item|
+      nof += 1 if item.created_at.to_date.equal?(date)
+    end
+    nof
   end
 end
