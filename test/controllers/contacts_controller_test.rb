9296d1454d5f28657dc2bef207f3102420fa3059
Kevin <Kevin-te@t-online.de>
2021-09-06 09:21:55 -0700

    Add unittests contacts_controller_test.rb

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -3,23 +3,35 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
-
-  setup do
-    @contacts = Contact.create(name: "Johannes kann es", email: "KannEs@Johannes.de", message: "i am jo---han---es and show me what your got")
-  end
-
   test 'should get new' do
     get contacts_path
     assert_response :success
   end
 
   test 'Create Contact' do
-    post contacts_url, contacts: { contacts: { name: @contacts.name, email: @contacts.email, message: @contacts.message } }
-    assert_response :success
+    post contacts_url,
+         params: { contact: { name: 'Johannes kann es', email: 'KannEs@Johannes.de',
+                              message: 'i am jo---han---es and show me what your got' } }
+    assert Contact.find_by_email('KannEs@Johannes.de')
   end
 
-  test 'Create Contact and check contactable users' do
-
+  test 'Check validation from message length Contact' do
+    post contacts_url,
+         params: { contact: { name: 'Johannes kann es', email: 'KannGarnichtsEs@Johannes.de', message: 'i ' } }
+    assert_not Contact.find_by_email('KannGarnichtsEs@Johannes.de')
   end
 
+  test 'Check validation from email Contact' do
+    post contacts_url,
+         params: { contact: { name: 'Johannes kann es', email: 'KannGarnichtsEsJohannes.de',
+                              message: 'Ich möchte mich Bewwerben TROLLLLLLLL' } }
+    assert_not Contact.find_by_email('KannGarnichtsEsJohannes.de')
+  end
+
+  test 'Check validation from name Contact' do
+    post contacts_url,
+         params: { contact: { name: 'Jo', email: 'Peter@Johannes.de',
+                              message: 'Ich möchte mich Bewwerben TROLLLLLLLL' } }
+    assert_not Contact.find_by_email('Peter@Johannes.de')
+  end
 end
