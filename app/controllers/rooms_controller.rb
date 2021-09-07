c089c6e4d727ded3f3291d78c3bb2f29cfada26c
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-09-07 15:05:48 +0200

    adds rudementary chat room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -33,6 +33,11 @@ class RoomsController < ApplicationController
     end
   end
 
+  def show
+    @room_message = RoomMessage.new room: @room
+    @room_messages = @room.room_messages.includes(:user)
+  end
+
   protected
 
   def load_entities
