072e3da7da14c05fa9f4bed03bbedb0588b306ff
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-09-07 14:39:13 +0200

    adds room creation by adding simple_form gem

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,17 +1,56 @@
 class RoomsController < ApplicationController
 
-  before_action :require_login
-
-  private
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to login_path
-    end
-  end
+  before_action :require_login, :load_entities
 
   def index
     @rooms = Room.all
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new permitted_parameters
+
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update_attributes(permitted_parameters)
+      flash[:success] = "Room #{@room.name} was updated successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def load_entities
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
+  end
+
+private
+
+def require_login
+  unless logged_in?
+    flash[:error] = "You must be logged in to access this section"
+    redirect_to login_path
+  end
+end
+
 end
