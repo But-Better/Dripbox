23aa218e2ead71711739e34efa91e562b098c204
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-09-10 14:40:19 +0200

    adds username to message

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import * as jquery from "jquery"
 
 import "channels"
 import "../src/home"
