c5dac55427fe5a07bb9152182a59e442cf59d304
note <kitsu-geissler@t-online.de>
2021-09-10 20:13:56 +0200

    temp data_graphic

--- app/javascript/packs/statistics_scripts/upload_files_history.js
+++ app/javascript/packs/statistics_scripts/upload_files_history.js
@@ -1,13 +1,21 @@
-const data = [{'date':new Date('2014-11-01'),'value':12},
-  {'date':new Date('2014-11-02'),'value':18}]
+const gon_data = gon.upload_file_history;
+
+const data = [];
+
+for (let i = 0; i < gon_data.size; i++) {
+  data[i] = {"date": new Date(gon_data[i]["date"].toString()), "number": gon_data[i]["number"]};
+  console.log(data[i])
+}
+
 
 MG.data_graphic({
-  title: "Uploads",
-  description: "This graphic shows a time-series of your uploads.",
+  title: "Downloads",
+  description: "This graphic shows a time-series of downloads.",
+  data: data,
   width: 600,
   height: 250,
-  data: data,
   target: '#chart',
   x_accessor: 'date',
   y_accessor: 'number'
 })
+
