e63f0f4cd88ebf94b540846ff0ced716d8763ab1
MathisHTW <Mathis.Schott@Student.HTW-Berlin.de>
2021-09-10 14:21:18 +0200

    message should be removed from input field but actually isnt

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //= require_tree .
+//= require jquery3
+//= require popper
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
