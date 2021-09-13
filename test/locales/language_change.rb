9079c21f1af1e6537a36dd4aa3570f4b34b11a80
Kevin <Kevin-te@t-online.de>
2021-09-13 14:40:41 -0700

    add unittests

--- test/locales/language_change.rb
+++ test/locales/language_change.rb
@@ -21,4 +21,32 @@ class LanguageChange < ActionDispatch::IntegrationTest
     get "#{root_path}?locale=dasdad"
     assert_select 'h2', 'Please sign in'
   end
+
+  test "change language and redirect to another pages" do
+
+    text1 = "Neueste Uploads:"
+    #<div id="heading2">
+    #<h2>Neueste Uploads:</h2>
+    #</div>
+
+    text2 = "Kennzeichnungen:"
+    #<div id="heading2">
+    #<h2>Kennzeichnungen:</h2>
+    #</div>
+
+    text3 = "Nichts ausgewählt"
+    #<div id="informationField">
+    #<p>Nichts ausgewählt</p>
+    #</div>
+
+    get "#{root_path}?locale=de"
+
+    post login_path, params: { user: { email: "note@mail.com", password: "123456789asdfghxA" } }
+
+    assert_select 'div#recentUploadsSection>div#heading2>h2', text1
+
+    assert_select 'div#categorySection>div#heading2>h2', text2
+
+    assert_select 'div#contentField>div#heading2>h2', text3
+  end
 end
