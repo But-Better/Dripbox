9f76f92b28919a2db07e7c69fb1148cbfb9e0967
note <kitsu-geissler@t-online.de>
2021-09-18 21:52:53 +0200

    change layout to better use given space

--- app/javascript/packs/statistics_scripts/file_size_classification.js
+++ app/javascript/packs/statistics_scripts/file_size_classification.js
@@ -11,9 +11,9 @@ MG.data_graphic({
   title: 'Top 5 Files in Terms of Size',
   data: data,
   chart_type: 'bar',
-  y_accessor: 'size',
-  x_accessor: 'file',
-  height: 200,
+  x_accessor: 'size',
+  y_accessor: 'file',
+  height: 500,
   full_width: true,
   target: '#file_size_classification',
 })
