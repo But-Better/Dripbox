085f7f9c324407c3e77e1e36cee2466b6ef2ce50
Mathis <mathis.schott@htw-berlin.de>
2021-09-21 23:09:24 +0200

    adds restrictions to roomname and messages

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ApplicationRecord
   has_many :room_messages
+
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            length: { minimum: 1, maximum: 20 }
 end
--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,4 +1,8 @@
 class RoomMessage < ApplicationRecord
   belongs_to :room
   belongs_to :user
+
+  validates :message,
+            presence: true,
+            length: { minimum: 1, maximum: 500 }
 end
