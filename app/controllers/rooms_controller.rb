f84f525b2b27a20e5b627c646e55c8fffe037e51
Mathis <mathis.schott@htw-berlin.de>
2021-09-21 23:31:26 +0200

    reformat

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,66 +1,66 @@
 class RoomsController < ApplicationController
 
-  before_action :load_params , :not_logged_in
+  before_action :load_params, :not_logged_in
 
-#load all existing Chatrooms for display
-def index
-  @existingRooms = Room.all
-end
+  #load all existing Chatrooms for display
+  def index
+    @existingRooms = Room.all
+  end
 
-#load data of selected Chatroom
-def show
+  #load data of selected Chatroom
+  def show
 
-  @selectedRoom = Room.find(params[:id])
+    @selectedRoom = Room.find(params[:id])
 
-  @roomMessages = @selectedRoom.room_messages.all
-  @roomMessage = RoomMessage.new
+    @roomMessages = @selectedRoom.room_messages.all
+    @roomMessage = RoomMessage.new
 
-  @room_id = params[:id]
-end
+    @room_id = params[:id]
+  end
 
-def new
-  @room = Room.new
-  @nameConflict = false
-end
+  def new
+    @room = Room.new
+    @nameConflict = false
+  end
 
-def create
-  @room = Room.new(room_params)
+  def create
+    @room = Room.new(room_params)
 
-  #check if room name is unique
-  alreadyExistingRooms = Room.all
-  alreadyExistingRooms.each do |alreadyExistingRoom|
-    if @room.name == alreadyExistingRoom.name
-      @nameConflict = true
-      render :new
+    #check if room name is unique
+    alreadyExistingRooms = Room.all
+    alreadyExistingRooms.each do |alreadyExistingRoom|
+      if @room.name == alreadyExistingRoom.name
+        @nameConflict = true
+        render :new
+      end
+    end
+
+    if !@nameConflict
+      if @room.save
+        ActionCable.server.broadcast "rooms_channel", @room
+        redirect_to @room
+      else
+        render :new
+      end
     end
   end
 
-  if !@nameConflict
-    if @room.save
-      ActionCable.server.broadcast "rooms_channel", @room
-      redirect_to @room
-    else
-      render :new
-    end
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
-end
 
-private
-
-def room_params
-  params.require(:room).permit(:name)
-end
-
-def load_params
-  current_user
-  @current_user = User.find_by(id: session[:user_id])
-end
+  def load_params
+    current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 
   def not_logged_in
     if @current_user.nil?
       redirect_to login_path
     end
-    end
+  end
 
 end
 
