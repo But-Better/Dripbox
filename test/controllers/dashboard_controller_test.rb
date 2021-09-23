469e9f3ef852a7b407e214cf22820ae47391b925
note <kitsu-geissler@t-online.de>
2021-09-23 10:16:49 +0200

    fix dashboard controller test after last rearrangement

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   setup do
-    password = "123456789asdfghxA"
-    @user = User.create(username: 'note', email: 'note@mail.com', password: password, email_confirmed: true,
+    @password = "123456789asdfghxA"
+    @user = User.create(username: 'note', email: 'note@mail.com', password: @password, email_confirmed: true,
                         confirm_token: nil)
     @test_resource = @user.user_resources.create(name: 'file', desc: 'eleven', created_at: "2021-09-06 11:42:29.946328")
     @test_resource.file.attach(io: File.open('app/assets/images/placeholder.svg'), filename: 'file.jpg')
@@ -13,24 +13,24 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
   end
 
   def login
-    post login_url params: { email: @user[:email], password: password }
+    post login_url params: { email: @user[:email], password: @password }
   end
 
-  test 'should get index' do
+  test 'should get index with login' do
     login
     get dashboard_url
     assert_response :success
-  end
-
-  test 'with login doesnt redirect' do
-    login
-    get dashboard_url
   end
 
   test 'should get index but with argument' do
     login
     get dashboard_url(UserResource.first)
     assert_response :success
+  end
+
+  test 'should redirect on index with argument without login' do
+    get dashboard_url(UserResource.first)
+    assert_redirected_to login_url params: { locale: 'en' }
   end
 
   test 'redirect to login page if not logged in' do
