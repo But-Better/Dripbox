621399ab5553b46bd887a590ebe62f414e751297
note <kitsu-geissler@t-online.de>
2021-09-27 23:33:14 +0200

    "Expected property shorthand" fix test

--- app/javascript/packs/statistics_scripts/upload_files_history.js
+++ app/javascript/packs/statistics_scripts/upload_files_history.js
@@ -1,23 +1,21 @@
-import * as MG from 'metrics-graphics'
-import * as d3 from 'd3'
+import * as MG from "metrics-graphics";
+import * as d3 from "d3";
 
 /* global MG, gon */
 // noinspection JSUnresolvedVariable
 const data = MG.convert.date(gon.upload_file_history, 'date');
 
-/* eslint-env es6 */
+/* eslint object-shorthand: ["error", "always", { "avoidQuotes": true }] */
 // noinspection JSUnresolvedFunction
-MG.data_graphic(
-  {
-    title: "Upload History Graph",
-    data: data,
-    interpolate: d3.curveLinear,
-    missing_is_zero: true,
-    full_width: true,
-    height: 200,
-    right: 40,
-    y_accessor: "number",
-    target: "#upload_file_history",
-    x_extended_ticks: true
-  }
-);
+MG.data_graphic({
+  title: "Upload History Graph",
+  data: data,
+  interpolate: d3.curveLinear,
+  missing_is_zero: true,
+  full_width: true,
+  height: 200,
+  right: 40,
+  y_accessor: "number",
+  target: "#upload_file_history",
+  x_extended_ticks: true
+});
