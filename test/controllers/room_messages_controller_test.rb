1ffe95ffb83a941fcf59b8294eb67caaa1025a1e
Mathis <mathis.schott@htw-berlin.de>
2021-10-01 11:47:25 +0200

    snake_case convention for room_messages_controller_test.rb

--- test/controllers/room_messages_controller_test.rb
+++ test/controllers/room_messages_controller_test.rb
@@ -4,37 +4,37 @@ require 'test_helper'
 
 class RoomMessagesControllerTest < ActionDispatch::IntegrationTest
   def login
-    post '/login', params: { email: @testUser1.email, password: '123456789asdfghxA' }
+    post '/login', params: { email: @test_user1.email, password: '123456789asdfghxA' }
   end
 
   setup do
-    @testUser1 = User.new(username: 'User1', email: Faker::Internet.email, password: '123456789asdfghxA', email_confirmed: true,
-                          confirm_token: nil)
-    assert @testUser1.save
+    @test_user1 = User.new(username: 'User1', email: Faker::Internet.email, password: '123456789asdfghxA', email_confirmed: true,
+                           confirm_token: nil)
+    assert @test_user1.save
 
-    @testUser2 = User.new(username: 'User2', email: Faker::Internet.email, password: '123456789asdfghxB', email_confirmed: true,
-                          confirm_token: nil)
-    assert @testUser2.save
+    @test_user2 = User.new(username: 'User2', email: Faker::Internet.email, password: '123456789asdfghxB', email_confirmed: true,
+                           confirm_token: nil)
+    assert @test_user2.save
 
-    @testRoom1 = Room.new(name: 'testRoom1')
-    assert @testRoom1.save
+    @test_room1 = Room.new(name: 'testRoom1')
+    assert @test_room1.save
   end
 
   test 'message creation' do
-    numberExistingMessages = RoomMessage.count
+    number_existing_messages = RoomMessage.count
     # empty messages are not saved and reload site
-    post rooms_path(id: @testRoom1.id), params: { room_message: { message: '' } }
-    assert numberExistingMessages == RoomMessage.count
+    post rooms_path(id: @test_room1.id), params: { room_message: { message: '' } }
+    assert number_existing_messages == RoomMessage.count
     assert_response :redirect
 
     # messages that are too long also arent saved
-    tooLongMessage = 'c' * 501
-    post rooms_path(id: @testRoom1.id), params: { room_message: { message: tooLongMessage } }
-    assert numberExistingMessages == RoomMessage.count
+    too_long_message = 'c' * 501
+    post rooms_path(id: @test_room1.id), params: { room_message: { message: too_long_message } }
+    assert number_existing_messages == RoomMessage.count
     assert_response :redirect
 
     # valid messages are saved and reload site
-    post rooms_path(id: @testRoom1.id), params: { room_message: { message: 'unique Message 1239873498' } }
+    post rooms_path(id: @test_room1.id), params: { room_message: { message: 'unique Message 1239873498' } }
     #assert RoomMessage.find_by_message('unique Message 1239873498')
     #assert (numberExistingMessages + 1) == RoomMessage.count
     assert_response :redirect
