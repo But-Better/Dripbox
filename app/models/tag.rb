4c16aefabae69a364c9f152c34fefc3cfacb38aa
note <kitsu-geissler@t-online.de>
2021-10-03 00:06:37 +0200

    add validation to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,11 @@
 
 class Tag < ApplicationRecord
   has_and_belongs_to_many :user_resources
+
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            length: { minimum: 1, maximum: 30 },
+            allow_nil: false,
+            allow_blank: false
 end
